8520e286b917e5fbc2da685bed421d1c4647fe00
haritonov.aka@gmail.com <03Feb1987>
2019-04-19 21:46:27 +0300

    front model

--- frontend/src/components/shop/ProductCard.vue
+++ frontend/src/components/shop/ProductCard.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="uk-card uk-card-small uk-card-default pub-product-card">
-        <div class="uk-card-media-top uk-cover-container uk-height-medium">
+        <div class="uk-card-media-top uk-cover-container uk-height-medium uk-position-relative">
             <img :src="product.image" class="uk-align-center" :alt="product.name" uk-cover>
+            <div class="pub-product-ingridients uk-flex uk-flex-middle uk-flex-center uk-flex-wrap uk-flex-wrap-middle">
+                <div v-for="ig in product.ingredient_list" :key="ig.id" class="pub-ig">
+                    {{ig.name}}
+                </div>
+            </div>
         </div>
         <div class="uk-card-body uk-text-center uk-padding-remove">
             <span class="pub-product-name">{{product.name}}</span>
